68312b3d117f2ef67cc7c02e8aa69969fb9048ae
zacjoe <jwangzi@outlook.com>
2013-09-19 23:05:59 -0700

    testing logo size on ipad

---
+++ assets/js/gmc.js
@@ -0,0 +1,17 @@
+
+
+// $( document ).ready(function() {
+
+
+
+// 	$(window).resize(function() {
+
+// 	});
+
+
+// 	function resizeLogo() {
+// 		if ($(window).width() < 1200) {
+			
+// 		}
+// 	}
+// });
