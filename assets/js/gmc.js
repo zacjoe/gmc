a26fea5a83bc4136638d079f5c7c5c4860242b91
zacjoe <jwangzi@outlook.com>
2013-09-20 02:22:38 -0700

    gmc change

--- assets/js/gmc.js
+++ assets/js/gmc.js
@@ -19,10 +19,12 @@ window.onload = function(){
 function resizeLogo() {
 		
 		if ($(window).width() < 420) {
-			$('.logo img').width(100);
-			$('.logo img').css('top', '5px');
+			$('.logo img').width(150);
+			$('.logo img').css('top', '15px');
+			$('.logo h6').css('font-size', "8px");
 		} else {
-			$('.logo img').width("auto");
+			$('.logo img').width(220);
 			$('.logo img').css('top', '-25px');
+			$('.logo h6').css('font-size', "10px");
 		}
 }
