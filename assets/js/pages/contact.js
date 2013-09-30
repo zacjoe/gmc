2fb39ad8d61b7934c3c815f4c5b4fb08c225a948
zacjoe <jwangzi@outlook.com>
2013-09-30 00:11:56 -0700

    updated contact page

--- assets/js/pages/contact.js
+++ assets/js/pages/contact.js
@@ -5,16 +5,31 @@ var Contact = function () {
         //Map
         initMap: function () {
 			var map;
+			var map_us;
 			$(document).ready(function(){
 			  map = new GMaps({
 				div: '#map',
 				lat: 3.1708333,
 				lng: 101.6538889
 			  });
+
+			  map_us = new GMaps({
+			  	div: '#map_us',
+			  	lat: 32.90385,
+			  	lng: -117.118597,
+			  	zoom: 11
+			  });
+
 			   var marker = map.addMarker({
 		            lat: 3.1708333,
 					lng: 101.6538889,
-		            title: 'Techtual Consulting'
+		            title: 'Techtual Consulting Malaysia'
+		        });
+
+			   var marker_us = map_us.addMarker({
+		            lat: 32.90385,
+					lng: -117.118597,
+		            title: 'Techtual Consulting U.S.A'
 		        });
 			});
         }
